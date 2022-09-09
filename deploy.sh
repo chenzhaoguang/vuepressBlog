cdd835434b4cac66f9efbfb0f23099d9fc7f9084
chenzhaoguang <chenzhaoguang@zhongan.com>
2022-09-09 19:09:47 +0800

    feat: init blog

--- deploy.sh
+++ deploy.sh
@@ -10,13 +10,13 @@ npm run build
 cd dist
 
 # 如果是发布到自定义域名
-echo 'www.thyiad.top' > CNAME
+# echo 'www.keepjs.top' > CNAME
 
 git init
 git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io
-git push -f git@github.com:Thyiad/thyiad.github.io.git master
+git push -f git@github.com:chenzhaoguang/chenzhaoguang.github.io.git master
 
 cd -
